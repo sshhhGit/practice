578724aae1f3aaab839471c44cbddc42deca54c5
sshhhgit <seungho0497@hanmail.net>
2021-06-21 18:12:53 +0900

    new

---
+++ src/bj/Main1110.java
@@ -0,0 +1,18 @@
+package bj;
+
+import java.util.Scanner;
+
+public class Main1110 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        while (true) {
+            int a = (N%100-N%10)/10+N%10;
+            int b = (N%10)*10+a;
+            }
+
+
+
+        }
+    }
+
