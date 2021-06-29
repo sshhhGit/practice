e2d2726affad67105e9e24591d0140dba3b94e78
Seungho <83917282+sshhhGit@users.noreply.github.com>
2021-06-29 16:28:42 +0900

    Add files via upload

---
+++ src/thisisjava/WrongPasswordException.java
@@ -0,0 +1,8 @@
+package asdf;
+
+public class WrongPasswordException extends Exception{
+    public WrongPasswordException() {}
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
